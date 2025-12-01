ee9f111dfdafbc668b86d028af1580bdac382f4f
101Krishna <92878564+101Krishna@users.noreply.github.com>
2025-12-01 16:26:50 +0530

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import './Header.css';
+
+const Header = () => {
+  const cartItems = useSelector(state => state.cart.items);
+  const totalItems = cartItems.reduce((sum, item) => sum + item.quantity, 0);
+
+  return (
+    <header className="header">
+      <div className="header-container">
+        <Link to="/" className="logo">
+          <h1>🌿 GreenLeaf Store</h1>
+        </Link>
+        <nav className="nav">
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/products" className="nav-link">Products</Link>
+          <Link to="/cart" className="cart-link">
+            <span className="cart-icon">🛒</span>
+            <span className="cart-count">{totalItems}</span>
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
